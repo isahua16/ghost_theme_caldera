643d5e3451df7c6f045d4a3e61e0c1c5bfe5ef9b
isahua16 <isael.huard@calderainteractive.com>
2023-11-20 21:51:46 -0700

    bunch of stuff

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,32 @@ import "../css/index.css";
 import menuOpen from "./menuOpen";
 import infiniteScroll from "./infiniteScroll";
 
-
 // Call the menu and infinite scroll functions
 menuOpen();
 infiniteScroll();
+
+function send_to_steam() {
+    window.open("https://store.steampowered.com/app/2514330/The_Rabbit_Haul/");
+}
+
+function send_to_blog() {
+    window.location.href = "http://localhost:2371/blog/ ";
+}
+
+if (document.querySelector(`#wishlist-button`)) {
+    document
+        .querySelector(`#wishlist-button`)
+        .addEventListener(`click`, send_to_steam);
+}
+
+if (document.querySelector(`#all_posts_button`)) {
+    document
+        .querySelector(`#all_posts_button`)
+        .addEventListener(`click`, send_to_blog);
+}
+
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset();
+    }
+};
